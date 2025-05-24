c1f144a4dba5a80c16343b9753bf1fe07893fccd
Mitieee <gabrielamitie@gmail.com>
2025-05-24 14:14:11 -0300

    Deploy inicial

--- db.php
+++ db.php
@@ -1,11 +1,14 @@
 <?php
 $url = getenv("JAWSDB_URL");
 $dbparts = parse_url($url);
-$host = $dbparts['qn66usrj1lwdk1cc.cbetxkdyhwsb.us-east-1.rds.amazonaws.com'];
-$user = $dbparts['bcgq92tgn5llinno'];
-$pass = $dbparts['g8buzhryqff6f4m'];
-$dbname = ltrim($dbparts['jl3ov1glq3jyb7au'], '/');
+
+$host = $dbparts['host'];       
+$user = $dbparts['user'];       
+$pass = $dbparts['pass'];       
+$dbname = ltrim($dbparts['path'], '/'); 
+
 $conn = new mysqli($host, $user, $pass, $dbname);
+
 if ($conn->connect_error) {
     die("Erro na conexão com o banco de dados: " . $conn->connect_error);
 }
