2d7f679de7e0a62330460e247916ceb9452c89df
Mitieee <gabrielamitie@gmail.com>
2025-05-24 11:25:20 -0300

    Atualiza conexão com banco JawsDB

--- db.php
+++ db.php
@@ -1,10 +1,12 @@
 <?php
-$servername = "localhost";
-$username = "root"; 
-$password = ""; 
-$dbname = "emergencias";
-$conn = new mysqli($servername, $username, $password, $dbname);
+$url = getenv("JAWSDB_URL");
+$dbparts = parse_url($url);
+$host = $dbparts['qn66usrj1lwdk1cc.cbetxkdyhwsb.us-east-1.rds.amazonaws.com'];
+$user = $dbparts['bcgq92tgn5llinno'];
+$pass = $dbparts['g8buzhryqff6f4m'];
+$dbname = ltrim($dbparts['jl3ov1glq3jyb7au'], '/');
+$conn = new mysqli($host, $user, $pass, $dbname);
 if ($conn->connect_error) {
-    die("Falha" . $conn->connect_error);
+    die("Erro na conexão com o banco de dados: " . $conn->connect_error);
 }
 ?>
